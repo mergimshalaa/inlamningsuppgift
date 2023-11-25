91dc9c15043703862ff16b8c75b17ddd7beb2e4a
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-11-25 15:26:02 +0100

    setup project, fix test error

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Form from "./Components/Form/Form";
+
+function App() {
+
+  return (
+    <div className="dictionary-app">
+      <header>
+        <h1 className="app-header">Dictionary App</h1>
+      </header>
+      <main>
+        <Form />
+      </main>
+  </div>
+  )
+}
+
+export default App;
