e92951a4344118429812d492d3f5a3b1b85d20b6
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-11-28 20:00:00 +0100

    set input as empty after search, fetching all data now

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,14 +1,14 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import { IApiResponse } from "../../interfaces/IApiResponse.interface";
 import { IErrResponseMessage } from "../../interfaces/IErrResponseMessage.interface";
-import Definitions from "../Definition";
+import Definitions from "../Definition/Definition";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import PhoneticsSection from "../Phonetics/Phonetics";
 import "./Form.css";
 
 export default function Form() {
   const [word, setWord] = useState("");
-  const [response, setResponse] = useState<IApiResponse | null>(null);
+  const [response, setResponse] = useState<IApiResponse[] | null>(null);
   const [error, setError] = useState<IErrResponseMessage | null>(null);
   const [showDefinition, setShowDefinition] = useState(false);
 
@@ -17,22 +17,27 @@ export default function Form() {
       if (!word.trim()) throw new Error("Empty Search");
   
       const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-      const data: IApiResponse[] = await response.json();
+      const data = await response.json();
   
       if (data?.length) {
-        setResponse(data[0]);
+        setResponse(data);
         setShowDefinition(true);
         setError(null);
       } else {
-        throw new Error("No Definitions Found");
+        setResponse(null);
+        setError({
+          title: "No Definitions Found",
+          message: "Sorry pal, we couldn't find definitions for the word you were looking for.",
+          resolution: "You can try the search again at a later time or head to the web instead.",
+        });
       }
     } catch (error) {
       setResponse(null);
       setShowDefinition(false);
       setError({
-        title: "No Definitions Found",
-        message: "Sorry pal, we couldn't find definitions for the word you were looking for.",
-        resolution: "You can try the search again at a later time or head to the web instead.",
+        title: "Empty Search",
+        message: "Sorry buddy, you can't search for an empty word.",
+        resolution: "You can try the search again at a later time.",
       });
     }
   };
@@ -44,6 +49,7 @@ export default function Form() {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     fetchData();
+    setWord("");
   };
 
   return (
@@ -62,33 +68,36 @@ export default function Form() {
         </div>
       </section>
       {error && <ErrorMessage title={error.title} message={error.message} resolution={error.resolution} />}
+
       {showDefinition && response && (
-        <div>
+        response.map((result: IApiResponse, index: number) => ( 
+        <div data-testid="search-word">
           <section className="dictionary">
             <div>
               <div>
                 <div style={{ textAlign: "center" }}>
-                  <p style={{ fontSize: "2rem" }}>{response.word}</p>
+                  <p style={{ fontSize: "2rem" }}>{result.word}</p>
                   <hr style={{ marginBottom: "1rem" }} />
                 </div>
-                {response.meanings.map((meaning, index) => (
+                {result.meanings.map((meaning, index) => (
             <Definitions key={index} meaning={meaning} />
           ))}
               </div>
             </div>
-          </section>
-
-          {response.phonetics?.length > 0 && (
-            <section className="section-phonetics dictionary">
-              <div className="phonetics-container">
+            <hr style={{ marginTop: "2rem" }} />
+            {result.phonetics?.length > 0 && (
+            
+              <div style={{marginTop: '3rem'}}>
                 <PhoneticsSection
-                  word={response.word}
-                  phonetics={response.phonetics}
+                  word={result.word}
+                  phonetics={result.phonetics}
                 />
               </div>
-            </section>
           )}
+          </section>
+
         </div>
+        ))
       )}
     </div>
   );
